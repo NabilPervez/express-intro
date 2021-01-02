a3499050944577a61c2f56bbb32cf86e5b01cc83
Nabil Pervez <perveznabil@gmail.com>
2021-01-02 11:36:29 -0600

    wk1 assign

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+
+// import routers
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,7 +13,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+// assign
 app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
